25d5dc4974a69d38a3fa701ba35ff20cde55707d
Nipun Garg <nipung271@gmail.com>
2020-03-10 03:09:32 +0530

    Decouple the vertex input from the shader code

--- app.h
+++ app.h
@@ -4,10 +4,13 @@
 #include "vulkan/include/vulkan/vulkan.h"
 #include "window.h"
 
+#include <array>
 #include <iostream>
 #include <optional>
 #include <stdexcept>
 #include <memory>
+
+#include <glm/glm.hpp>
 
 struct QueueFamilyIndices {
     std::optional<uint32_t> graphicsQueue;
@@ -23,6 +26,34 @@ struct SwapchainSupportDetails {
     VkSurfaceCapabilitiesKHR capabilities;
     std::vector<VkSurfaceFormatKHR> formats;
     std::vector<VkPresentModeKHR> presentModes;
+};
+
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription bindingDescription = {};
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(Vertex);
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+        return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions = {};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+    }
 };
 
 class VulkanSampleApp {
