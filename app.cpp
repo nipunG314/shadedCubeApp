4471619964feaf7b5f60c6e38d4c86b2c6679577
Nipun Garg <nipung271@gmail.com>
2020-03-10 02:50:32 +0530

    Create Pipeline Shader Stages for Modiles

--- app.cpp
+++ app.cpp
@@ -412,6 +412,23 @@ void VulkanSampleApp::createGraphicsPipeline() {
     auto vertShaderModule = createShaderModule(device, "shaders/vert.spv");
     auto fragShaderModule = createShaderModule(device, "shaders/frag.spv");
 
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = vertShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStageInfos[] = {
+        vertShaderStageInfo,
+        fragShaderStageInfo
+    };
+
     vkDestroyShaderModule(device, fragShaderModule, nullptr);
     vkDestroyShaderModule(device, vertShaderModule, nullptr);
 }
